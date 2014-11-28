4321e010cf1f52efee506be787270b7af7c4b327
William DeVore <william.quartz@gmail.com>
2014-11-27 22:06:18 -0500

    bumped up version to 0.9.5

--- lib/ranger/config.dart
+++ lib/ranger/config.dart
@@ -12,7 +12,7 @@ class CONFIG {
    * If you post a bug to the forum, please attach this flag.
    */
   static const String ENGINE_NAME = "Ranger-Dart";
-  static const String ENGINE_VERSION = "0.9.0";
+  static const String ENGINE_VERSION = "0.9.5";
   
   static const String CONFIG_OVERRIDE_FILE = r"resources/config.json";
   
